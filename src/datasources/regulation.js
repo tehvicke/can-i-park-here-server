8e9d449953479bbfe41933da6136db4190c2ce54
Viktor Dahl <tehvicke@gmail.com>
2020-04-08 15:34:49 +0200

    removed ugly date transformation needed due to wrong date format received from the client

--- src/datasources/regulation.js
+++ src/datasources/regulation.js
@@ -10,7 +10,7 @@ class Regulation {
 
     this.parkingAllowedTime = {
       start: undefined,
-      end: undefined
+      end: undefined,
     }
   }
 
@@ -41,15 +41,8 @@ class Regulation {
     const startWeekdayDiff = startWeekday - todayWeekday
     const endWeekdayDiff = endWeekday - todayWeekday
 
-    const startDateString = moment(usersTimeFormatted.format())
-      .add(startWeekdayDiff, 'd')
-      .format('YYYY-MM-DD')
-    const endDateString = moment(usersTimeFormatted.format())
-      .add(endWeekdayDiff, 'd')
-      .format('YYYY-MM-DD')
-
-    let startDate = moment(startDateString + ' ' + startTime, 'YYYY-MM-DD hhmm')
-    let endDate = moment(endDateString + ' ' + endTime, 'YYYY-MM-DD hhmm')
+    const startDate = moment(startTime.add(startWeekdayDiff, 'd')
+    const endDate = moment(endTime.format()).add(endWeekdayDiff, 'd')
 
     /* Adjust period to the week before if startTime is after the users time */
     if (usersTimeFormatted < startDate) {
